52c6d1446d421196e9036bb7fe01cd6773c81bd0
preshpi <egwuenuprecious7@gamil.com>
2022-08-18 12:28:17 +0100

    ee

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about pa-6">
+    <h1>About Todo app</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium, id tempora fuga ad itaque quam eaque eius sapiente et, fugit voluptate libero voluptatem cupiditate quibusdam beatae aliquid soluta aspernatur culpa!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque assumenda soluta provident quibusdam. Error, accusamus animi quae consequuntur et expedita nostrum porro ipsam esse veniam alias. Doloremque debitis voluptatem atque.</p>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deserunt eos asperiores expedita cupiditate molestiae aperiam exercitationem corrupti quos! Deleniti, accusantium?</p>
   </div>
 </template>
